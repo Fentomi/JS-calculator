ac14563d614ea3db659d0eebe6a6471e30d463aa
Fentomi <105577819+Fentomi@users.noreply.github.com>
2023-09-05 17:39:31 +0300

    calculator is working!

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ class UsefulFunctions {
         if(string[string.length-1] === '+' || string[string.length-1] === '-' || string[string.length-1] === '/' || string[string.length-1] === '*' || string[string.length-1] === '%') return true;
         return false; 
     }
+    static knifeString(string) {
+        let number = '';
+        let numbers = new Array();
+        let chars = new Array();
+    
+        for(let i = 0; i < string.length; i++) {
+            if(string[i] === '*' || string[i] === '/' || string[i] === '+' || string[i] === '-' || string[i] === '%' && i !== 0) {
+                numbers.push(number);
+                chars.push(string[i])
+                number = '';
+            } else number += string[i];
+        }
+        numbers.push(number);
+    
+        return { numbers, chars};
+    }
 }
 
 //basic class
@@ -32,6 +48,8 @@ class Calculator {
         this.btn_minus = document.getElementById('minus');
         this.btn_plus = document.getElementById('plus');
         this.btn_equal = document.getElementById('equal');
+        this.btn_point = document.getElementById('.');
+        this.btn_doubleZero = document.getElementById('00');
 
         //events
         this.btn_c.onclick = () => this.clearEnterScreen();
@@ -53,11 +71,10 @@ class Calculator {
         this.btn_multiply.onclick = () => this.addCharInEnterScreen('*');
         this.btn_minus.onclick = () => this.addCharInEnterScreen('-');
         this.btn_plus.onclick = () => this.addCharInEnterScreen('+');
-        this.btn_equal.onclick = () => this.addCharInEnterScreen('=');
-
-        this.enterString.onchange = () => { console.log('меня поменяли!'); }
+        this.btn_equal.onclick = () => this.calculateResult(this.enterString.textContent);
+        this.btn_point.onclick = () => this.addCharInEnterScreen('.');
     }
-
+    //add number in end enter-string
     addNumberInEnterScreen(number) {
         let string = this.enterString.textContent;
         if(string.length === 1 && string === '0'){
@@ -65,16 +82,29 @@ class Calculator {
         }
         this.enterString.textContent += number;
     }
-
+    //add char in end enter-string
     addCharInEnterScreen(char) {
         let string = this.enterString.textContent;
-        if(!UsefulFunctions.isCharNextTo(string) && char !== '-') this.enterString.textContent += char;
-        else if (string !== '0') this.enterString.textContent += '-';
-        else this.enterString.textContent = '-';
+        if(char === '.') {
+            let pointCount = 0;
+            let charCount = 0;
+            for(let i = 0; i < string.length; i++){
+                string[i] === '.' ? pointCount++ : pointCount;
+                string[i] === '+' || string[i] === '-' || string[i] === '/' || string[i] === '*' || string[i] === '%' ? charCount++ : charCount;
+            }
+            if(pointCount === charCount) this.enterString.textContent += '.';
+        }
+        else if(char === '-') {
+            if(string === '0') this.enterString.textContent = char;
+            else if(!UsefulFunctions.isCharNextTo(string)) this.enterString.textContent += char;
+        }
+        else {
+            if(!UsefulFunctions.isCharNextTo(string)) this.enterString.textContent += char;
+        }
     }
-
-    clearEnterScreen() { this.enterString.textContent = '0'; }
-
+    //clear enter-string
+    clearEnterScreen() { this.enterString.textContent = '0'; this.resultString.textContent = ''; }
+    //delete last symbol in enter-string
     delLastNumberInEnterScreen() { 
         if(this.enterString.textContent !== '0'){
             this.enterString.textContent = this.enterString.textContent.substring(0, this.enterString.textContent.length - 1);
@@ -83,8 +113,80 @@ class Calculator {
             this.enterString.textContent = '0'; 
         }
     }
-
-
+    //function-help with calculateResult
+    calcPara(char, temp = 0, numbers = [], result) {
+        if (result === 0) {
+            switch(char) {
+                case '+': result = Number(numbers[temp]) + Number(numbers[temp+1]); temp += 2; break;
+                case '-': result = Number(numbers[temp]) - Number(numbers[temp+1]); temp += 2; break;
+                case '*': result = Number(numbers[temp]) * Number(numbers[temp+1]); temp += 2; break;
+                case '/': result = Number(numbers[temp]) / Number(numbers[temp+1]); temp += 2; break;
+            }
+        }
+        else {
+            switch(char) {
+                case '+': result += Number(numbers[temp]); temp += 1; break;
+                case '-': result -= Number(numbers[temp]); temp += 1; break;
+                case '*': result *= Number(numbers[temp]); temp += 1; break;
+                case '/': result /= Number(numbers[temp]); temp += 1; break;
+            }
+        }
+        return { result, temp }
+    }
+    //calculate enter-string and set a result-string
+    calculateResult(string) {
+        const knife_string = UsefulFunctions.knifeString(string);
+        const chars = knife_string.chars;
+        const numbers = knife_string.numbers;
+        console.log(Number(numbers[0]) + Number(numbers[1]))
+    
+        let result = 0;
+        let temp = 0;
+    
+        chars.forEach(element => {
+            let resCalcPara = this.calcPara(element, temp, numbers, result);
+            result = resCalcPara.result;
+            temp = resCalcPara.temp;
+        })
+        
+        this.resultString.textContent = result;
+    }
 }
 
 const calculator = new Calculator();
+
+//interactive with user keyboard
+document.addEventListener('keydown', function(event) {
+    switch(event.code) {
+        case 'Minus': calculator.addCharInEnterScreen('-'); break;
+        case ('ShiftLeft' && 'Equal'): calculator.addCharInEnterScreen('+'); break;
+        case ('ShiftLeft' && 'Digit8'): calculator.addCharInEnterScreen('*'); break;
+        case 'Slash': calculator.addCharInEnterScreen('/'); break;
+
+        case 'Digit0': calculator.addNumberInEnterScreen('0'); break;
+        case 'Numpad0': calculator.addNumberInEnterScreen('0'); break;
+        case 'Digit1': calculator.addNumberInEnterScreen('1'); break;
+        case 'Numpad1': calculator.addNumberInEnterScreen('1'); break;
+        case 'Digit2': calculator.addNumberInEnterScreen('2'); break;
+        case 'Numpad2': calculator.addNumberInEnterScreen('2'); break;
+        case 'Digit3': calculator.addNumberInEnterScreen('3'); break;
+        case 'Numpad3': calculator.addNumberInEnterScreen('3'); break;
+        case 'Digit4': calculator.addNumberInEnterScreen('4'); break;
+        case 'Numpad4': calculator.addNumberInEnterScreen('4'); break;
+        case 'Digit5': calculator.addNumberInEnterScreen('5'); break;
+        case 'Numpad5': calculator.addNumberInEnterScreen('5'); break;
+        case 'Digit6': calculator.addNumberInEnterScreen('6'); break;
+        case 'Numpad6': calculator.addNumberInEnterScreen('6'); break;
+        case 'Digit7': calculator.addNumberInEnterScreen('7'); break;
+        case 'Numpad7': calculator.addNumberInEnterScreen('7'); break;
+        case 'Digit8': calculator.addNumberInEnterScreen('8'); break;
+        case 'Numpad8': calculator.addNumberInEnterScreen('8'); break;
+        case 'Digit9': calculator.addNumberInEnterScreen('9'); break;
+        case 'Numpad9': calculator.addNumberInEnterScreen('9'); break;
+
+        case 'Backspace': calculator.delLastNumberInEnterScreen(); break;
+        case 'KeyC': calculator.clearEnterScreen(); break;
+
+        case 'Enter': calculator.calculateResult(calculator.enterString.textContent); break;
+    }
+});
